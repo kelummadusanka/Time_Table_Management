2245c01f2067181a1fda02283c283f2ee000133d
kelummadusanka <79236557+kelummadusanka@users.noreply.github.com>
2022-08-15 19:36:02 +0530

    folder structre

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import React from 'react';
-import Nav from './Components/nav';
 import Login from './Components/Login/Login';
 import Dashboard from './Components/Lecturer/Dashboard/Dashboard';
 
@@ -8,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <Dashboard></Dashboard>
-      <Nav/>
       Test App for Time Table Mangement System
       <p>difudsiofudsifudsfuo</p>
       <Login />
