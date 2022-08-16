91f142562615014ff8bceb520a1d66d760615670
kelummadusanka <79236557+kelummadusanka@users.noreply.github.com>
2022-08-16 20:03:43 +0530

    create cards in admin dashboard

--- client/src/Components/Admin/AdminCrudCard/AdminCrudCard.jsx
+++ client/src/Components/Admin/AdminCrudCard/AdminCrudCard.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import './AdminCrudCard.css'
 import Rome from '../AdminImages/rome.jpg'
 
-function AdminCrudCard() {
+
+
+
+function AdminCrudCard(Card) {
   return (
     <div className="grid-item">
         <div className="card">
-          <img className="card-img" src={Rome} alt="Rome" />
+          <img className="card-img" src={Card.Image} alt="Rome" />
           <div className="card-content">
-            <h1 className="card-header">Time Table Management</h1>
-            <p className='card-text'>| manage all Time table from here!</p>
+            <h1 className="card-header">{Card.Header}</h1>
+            <p className='card-text'>{Card.Text}</p>
           </div>
         </div>
         </div>
--- client/src/Components/Admin/AdminCrudCards/AdminCrudCards.jsx
+++ client/src/Components/Admin/AdminCrudCards/AdminCrudCards.jsx
@@ -2,14 +2,34 @@ import React from 'react'
 import './AdminCrudCards.css'
 import AdminCrudCard from '../AdminCrudCard/AdminCrudCard'
 
+const Data =[
+  {
+    "Image":"F:\Fifth Semester 2021\Software project\Time_Table_Management\client\src\Components\Admin\AdminImages\grand-canyon.jpg",
+    "Header":"Time table Mangement",
+    "Text":"| manage all Time table from here!"
+  },
+  {
+    "Image":"F:\Fifth Semester 2021\Software project\Time_Table_Management\client\src\Components\Admin\AdminImages\grand-canyon.jpg",
+    "Header":"Lectures Mangement",
+    "Text":"| manage Lectures!"
+  },
+  {
+    "Image":"F:\Fifth Semester 2021\Software project\Time_Table_Management\client\src\Components\Admin\AdminImages\grand-canyon.jpg",
+    "Header":"Student Mangement",
+    "Text":"| manage Students!"
+  }
+]
+
 function AdminCrudCards() {
   return (
     <div className='CardsContainer'>
-      
-      <AdminCrudCard/>
-      <AdminCrudCard/>
-      <AdminCrudCard/> 
-      <AdminCrudCard/>       
+      {Data.map((Card,key) =>{
+
+        return(
+        <AdminCrudCard key={key} Text={Card.Text} Header = {Card.Header} Image = {Card.Image}/>)
+
+      })}
+           
       </div>
   )
 }
