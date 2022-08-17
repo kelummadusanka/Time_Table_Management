bfd0290e460859e3de257894f117924ce0172c1a
uvindu pabasara <uvindupabasara@gmail.com>
2022-08-17 21:56:11 +0530

    modify nav bar

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -1,145 +1,10 @@
-// import React from "react";
-// import Form from "react-bootstrap/Form";
-// import Button from "react-bootstrap/Button";
-// import { useState, useEffect } from "react";
 
-// function Login() { 
-//     const initialValues = { username: "", email: "", password: "" }; 
-//     const [formValues, setFormValues] = useState(initialValues); 
-//     const [formErrors, setFormErrors] = useState({}); 
-//     const [isSubmit, setIsSubmit] = useState(false); 
-
-//     const handleChange = (e) => { 
-//         const { name, value } = e.target; 
-//         setFormValues({ ...formValues, [name]: value }); 
-//     };
-//     const handleSubmit = (e) => {
-//         e.preventDefault();
-//         setFormErrors(validate(formValues)); 
-//         setIsSubmit(true); 
-//     }; 
-//     useEffect(() => { 
-//         console.log(formErrors); 
-//         if (Object.keys(formErrors).length === 0 && isSubmit) {
-//             console.log(formValues); 
-//         } 
-//     }, [formErrors]); 
-//     const validate = (values) => { 
-//         const errors = {}; 
-//         const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i; 
-//         if (!values.username) { 
-//             errors.username = "Username is required!"; 
-//         } 
-//         if (!values.email) { errors.email = "Email is required!"; 
-//         } else if (!regex.test(values.email)) {
-//             errors.email = "This is not a valid email format!"; } 
-//         if (!values.password) { 
-//             errors.password = "Password is required"; 
-//         } else if (values.password.length < 4) { 
-//             errors.password = "Password must be more than 4 characters"; 
-//         } else if (values.password.length > 10) { 
-//             errors.password = "Password cannot exceed more than 10 characters"; 
-//         } 
-//         return errors; 
-//     }; 
-//     return ( 
-//     <div className="container"> 
-//     {/* {Object.keys(formErrors).length === 0 && isSubmit ?(
-//          <div className="ui message success">Signed in successfully</div> 
-//          ) : (
-//              <pre>{JSON.stringify(formValues, undefined, 2)}</pre> 
-//              )
-//              }  */}
-//              <Form onSubmit={handleSubmit}> 
-//              <Form.Group size="lg" controlId="username" >
-//              <Form.Label>Username</Form.Label>
-//           <Form.Control
-//             autoFocus
-//             type="username"
-//             value={Text}
-//             onChange={(e) => setusername(e.target.value)}
-//           />
-//              </Form.Group>
-//              <h1>LOGIN</h1> 
-//              <div className="ui divider"></div> 
-//              <div className="ui form"> 
-//              <div className="field"></div> 
-//                 <div>
-//                     <label>Username</label>
-//                             <input
-//                                     type="text"
-//                                     name="username"
-//                                     placeholder="Username"
-//                                     value={formValues.username}
-//                                     onChange={handleChange}
-//                                 />
-//                 </div>
-//                 <div>
-//                     <label>Password</label>
-//                         <input
-//                                 type="password"
-//                                 name="password"
-//                                 placeholder="Password"
-//                                 value={formValues.password}
-//                                 onChange={handleChange}
-//                             />
-//                         </div>
-//                     <button className="fluid ui button blue">Login</button>
-                    
-//                 </div>
-//             </Form>
-//         </div>
-//     ); 
-// }
-
-// import React, { useState } from "react";
-// import Form from "react-bootstrap/Form";
-// import Button from "react-bootstrap/Button";
-// import "./Login.css";
-
-// export default function Login() {
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-
-//   function validateForm() {
-//     return email.length > 0 && password.length > 0;
-//   }
-
-//   function handleSubmit(event) {
-//     event.preventDefault();
-//   }
-
-//   return (
-//     <div className="Login">
-//       <Form onSubmit={handleSubmit}>
-//         <Form.Group size="lg" controlId="email">
-//           <Form.Label>Email</Form.Label>
-//           <Form.Control
-//             autoFocus
-//             type="email"
-//             value={email}
-//             onChange={(e) => setEmail(e.target.value)}
-//           />
-//         </Form.Group>
-//         <Form.Group size="lg" controlId="password">
-//           <Form.Label>Password</Form.Label>
-//           <Form.Control
-//             type="password"
-//             value={password}
-//             onChange={(e) => setPassword(e.target.value)}
-//           />
-//         </Form.Group>
-//         <Button block="true" size="lg" type="submit" disabled={!validateForm()}>
-//           Login
-//         </Button>
-//       </Form>
-//     </div>
-//   );
-// }
 
 
 import React from 'react'
 import { useForm } from "react-hook-form";
+
+
 import {
   ThemeProvider,
   theme,
@@ -157,15 +22,18 @@ import {
   Input,
   Stack,
   Checkbox,
-  Button
+  Button,
+  FormErrorMessage
 } from '@chakra-ui/react'
 
-const VARIANT_COLOR = 'teal'
+const VARIANT_COLOR = 'teal';
 
 
 const Login = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm(); 
+     const { register, handleSubmit, formState: { errors } } = useForm(); 
+    
   return (
+    
     <><Flex minHeight='100vh' width='full' align='center' justifyContent='center'>
     <Box 
       borderWidth={1}
@@ -189,14 +57,17 @@ const Login = () => {
       <FormControl>
         <FormLabel>Username</FormLabel>
         <Input type='email' placeholder='Enter your username' />
-        {/* {...register("firstname", { required: true })} */}
-      </FormControl>
+        <div className="container mt-2">
       
+    </div>
+      </FormControl>
+      <FormErrorMessage>(formik.errors.username)</FormErrorMessage>
 
       <FormControl mt={4}>
         <FormLabel>Password</FormLabel>
         <Input type='password' placeholder='Enter your password' />
       </FormControl>
+      
 
       <Stack isInline justifyContent='space-between' mt={4}>
           <Box>
@@ -213,6 +84,9 @@ const Login = () => {
       </Box>
     </Box>
   </Flex></>
+
+  
+  
   
   )
 }
