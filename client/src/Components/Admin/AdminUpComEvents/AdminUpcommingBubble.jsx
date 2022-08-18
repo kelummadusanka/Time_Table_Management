0dd01d09db9756860b0ece6f62c62c9fe88b5a57
kelummadusanka <79236557+kelummadusanka@users.noreply.github.com>
2022-08-19 00:30:32 +0530

    create lectrure

--- client/src/Components/Admin/AdminUpComEvents/AdminUpcommingBubble.jsx
+++ client/src/Components/Admin/AdminUpComEvents/AdminUpcommingBubble.jsx
@@ -1,20 +1,18 @@
 import React from 'react'
 import './AdminUpcommingBubble.css'
-import { FaBeer } from "@react-icons/all-files/fa/FaBeer";
-
-function AdminUpcommingBubble() {
+function AdminUpcommingBubble(props) {
   return (
          <div className="MA-History">
          <table className="MA-history-table">
            <tbody>
                  <tr >
-                   <td>20120/12/05</td>
-                   <td>09:30-10:30</td>
-                   <td>Convercation</td>
-                   <td>Place1</td>
-                   <td>Control System</td>
+                   <td>props.Date</td>
+                   <td>props.Time</td>
+                   <td>props.Description</td>
+                   <td>props.Place</td>
+                   <td>props.</td>
                    <td>
-                   <FaBeer />
+                    delete icon
                    </td>
                  </tr>
             
