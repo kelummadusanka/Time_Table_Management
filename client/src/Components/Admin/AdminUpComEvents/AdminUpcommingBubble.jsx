3d07e00cb4884eb89dea6fffb47d8368b65448ab
tilanga dulshan <tilangadulshan2@gmail.com>
2022-08-19 21:22:45 +0530

    create student ui

--- client/src/Components/Admin/AdminUpComEvents/AdminUpcommingBubble.jsx
+++ client/src/Components/Admin/AdminUpComEvents/AdminUpcommingBubble.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './AdminUpcommingBubble.css'
-import { FaBeer } from "@react-icons/all-files/fa/FaBeer";
 
 function AdminUpcommingBubble() {
   return (
@@ -14,7 +13,7 @@ function AdminUpcommingBubble() {
                    <td>props.Place</td>
                    <td>props.</td>
                    <td>
-                   <FaBeer />
+                    
                    </td>
                  </tr>
             
--- client/src/Components/Student/StudentDashboard/StudentDashboard.jsx
+++ client/src/Components/Student/StudentDashboard/StudentDashboard.jsx
@@ -1,8 +1,28 @@
+import { Box, HStack, Stack, VStack } from '@chakra-ui/react'
 import React from 'react'
-
+import { Image } from '@chakra-ui/react'
 function StudentDashboard() {
   return (
-    <div>Student Dashboard is here!</div>
+    <div>
+ 
+      <HStack spacing='24px' >
+        <VStack h={'calc(100vh)'} bg={'yellow'} flex={1} bgGradient={[
+    'linear(t-b, white.1000, yellow.30)',
+  ]} >
+          <div>
+          <Image
+            borderRadius='full'
+            boxSize='150px'
+            src='https://bit.ly/dan-abramov'
+            alt='Dan Abramov'
+            />
+          </div>
+        </VStack>
+        <VStack h={'calc(100vh)'} flex={4} bg={'blue.600'}></VStack>
+      </HStack>
+      <HStack height={200} bg={'brown'}>
+        </HStack>
+      </div>
   )
 }
 
