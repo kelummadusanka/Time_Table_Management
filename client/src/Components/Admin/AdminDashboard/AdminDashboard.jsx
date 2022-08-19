0ebfd025ee8d4cdb33bbd17edf881a2a51693ede
kelummadusanka <79236557+kelummadusanka@users.noreply.github.com>
2022-08-20 01:04:03 +0530

    add mngCard.jsx

--- client/src/Components/Admin/AdminDashboard/AdminDashboard.jsx
+++ client/src/Components/Admin/AdminDashboard/AdminDashboard.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import AdminCrudCards from '../AdminCrudCards/AdminCrudCards'
 import { Heading } from '@chakra-ui/react'
-import AdminUpcommingBubble from '../AdminUpComEvents/AdminUpcommingBubble'
+import AdminUpcommingItem from '../AdminUpComEvents/AdminUpcommingItem'
 
 function AdminDashboard() {
   return (
     <div>
       <div><AdminCrudCards/></div>
       <Heading size='lg' fontSize='50px' mt={10}>Upcomming Events </Heading>
-      <AdminUpcommingBubble/>
+      <AdminUpcommingItem/>
 
     </div>
 
---
+++ client/src/Components/Admin/AdminUpComEvents/AdminUpcommingItem.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './AdminUpcommingItem.css'
+
+function AdminUpcommingItem() {
+  return (
+         <div className="MA-History">
+         <table className="MA-history-table">
+           <tbody>
+                 <tr >
+                   <td>props.Date</td>
+                   <td>props.Time</td>
+                   <td>props.Description</td>
+                   <td>props.Place</td>
+                   <td>props.</td>
+                   <td>
+
+                   </td>
+                 </tr>
+            
+             
+           </tbody>
+         </table>
+       </div>
+  )
+}
+
+export default AdminUpcommingItem
