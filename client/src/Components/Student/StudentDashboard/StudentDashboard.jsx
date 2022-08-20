e2d5ac82f0f49d6b833e69d98bcfa6b815a1e6b7
tilanga dulshan <tilangadulshan2@gmail.com>
2022-08-20 13:05:40 +0530

    student Vstack decorations

--- client/src/Components/Student/StudentDashboard/StudentDashboard.jsx
+++ client/src/Components/Student/StudentDashboard/StudentDashboard.jsx
@@ -6,15 +6,23 @@ function StudentDashboard() {
     <div>
  
       <HStack spacing='24px' >
-        <VStack h={'calc(100vh)'} bg={'yellow'} flex={1} bgGradient='linear(red.100 0%, orange.100 25%, yellow.100 50%)' >
+        <VStack h={'calc(100vh)'} flex={1} borderBottom='8px' borderLeft='8px' borderBottomRadius={'lg'} borderBottomColor={'green.400'} borderLeftColor={'green.100'} bgGradient='linear(to-b, white, green.200)' >
           <div>
           <Image
+            border='8px'
+            borderColor={'green.100'}
             borderRadius='full'
-            boxSize='150px'
+            boxSize='350px'
             src='https://bit.ly/dan-abramov'
             alt='Dan Abramov'
             />
           </div>
+          <Box width={'full'} textAlign={'center'} border='2px' margin='40' h={'20'} fontSize='50px' fontWeight={'semibold'} fontStyle='italic' textColor={'blackAlpha.500'}>
+            Uvindu pabasara
+          </Box>
+          <Box width={'full'} textAlign={'center'} border='2px' margin='40' h={'20'} fontSize='50px' fontWeight={'semibold'} fontStyle='italic' textColor={'blackAlpha.500'}>
+            EG/2018/3403
+          </Box>
         </VStack>
         <VStack h={'calc(100vh)'} flex={4} bg={'blue.600'}></VStack>
       </HStack>
